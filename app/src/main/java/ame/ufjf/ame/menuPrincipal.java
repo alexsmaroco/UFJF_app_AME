47c000f9fdd11d8811c0201bc682017e48ea2fd0
Alex Maroco <alexsmaroco@gmail.com>
2017-06-12 17:39:55 -0300

    + arquivos

---
+++ app/src/main/java/ame/ufjf/ame/menuPrincipal.java
@@ -0,0 +1,13 @@
+package ame.ufjf.ame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class menuPrincipal extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_principal);
+    }
+}
